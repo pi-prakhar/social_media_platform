ddeb0ed126c2ecba2012c5a73a27933a43084bda
Prakhar Awasthi <pawasthi961@gmail.com>
2020-07-30 12:23:07 +0530

    1 commit

---
+++ server/app.js
@@ -0,0 +1,23 @@
+const express  = require('express')
+const app = express()
+const PORT = 5000
+
+const customMiddleware = (req,res,next)=>{
+    console.log("Middleware executed")
+    next()
+}
+
+
+app.get('/',(req,res) =>{
+    console.log("home")
+    res.send("hello  world")
+})
+
+app.get('/about',customMiddleware,(req,res) =>{
+    console.log("about")
+    res.send("about page")
+})
+
+app.listen(PORT,()=>{
+    console.log("server is running on",PORT)
+})
